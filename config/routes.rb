6838c319462242ffc5f9357d5704c7b24ac3539d
Muhammad Shariq <shariqmuhammad@yahoo.com>
2013-05-05 02:07:35 +0300

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp3::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
   
   root to: 'static_pages#home'
   
@@ -8,6 +10,10 @@ SampleApp3::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   
   
 
